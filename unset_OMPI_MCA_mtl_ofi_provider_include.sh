898ab7924435755f4fc705731975ab2c3f295223
nikishe <115556635+nikishe@users.noreply.github.com>
2025-01-16 11:17:27 +0000

    bau

--- unset_OMPI_MCA_mtl_ofi_provider_include.sh
+++ unset_OMPI_MCA_mtl_ofi_provider_include.sh
@@ -9,7 +9,7 @@
 module load OpenMPI/4.1.4-GCC-12.2.0
 
 unset OMPI_MCA_mtl_ofi_provider_include
-export OMPI_MCA_mtl_ofi_provider_include=ucx
+export OMPI_MCA_mtl_ofi_provider_include=udp
 
 
 #srun --export=ALL ./test_mpi
