0020feba7014e5c5e0a566315180900a8771813b
nikishe <115556635+nikishe@users.noreply.github.com>
2025-01-16 13:58:25 +0000

    addition of disable mtl

---
+++ disabled_ofi_mtl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --mem=4G
+#SBATCH --time=00:10:00
+
+
+
+module load OpenMPI/4.1.4-GCC-12.2.0
+
+export OMPI_MCA_mtl="^ofi"
+
+mpirun --mca btl tcp -np $SLURM_NTASKS ./test_mpi
